456b3d46ced162614ad90c0567159123f3872a01
Tainara Ribeiro <ribeiros.tai@gmail.com>
2025-07-18 15:31:50 -0300

    Update ContaRepository.java

--- TESTI/src/main/java/io/github/nicolazlxl/TesteSQLPOO/Conta/ContaRepository.java
+++ TESTI/src/main/java/io/github/nicolazlxl/TesteSQLPOO/Conta/ContaRepository.java
@@ -4,10 +4,27 @@
  */
 package io.github.nicolazlxl.TesteSQLPOO.Conta;
 
+import io.github.nicolazlxl.TesteSQLPOO.Entidades.Repository;
+
 /**
  *
  * @author taina
  */
-public class ContaRepository {
+public class ContaRepository 
+    extends Repository<Conta> {
+     @Override
+    public String getJpqlFindAll() {
+        return "SELECT a FROM Conta a";
+    }
+
+    @Override
+    public String getJpqlFindById() {
+        return "SELECT a FROM Conta a WHERE a.id = :id";
+    }
+
+    @Override
+    public String getJpqlDeleteById() {
+        return "DELETE FROM Conta a WHERE a.id = :id";
+    }
     
 }
