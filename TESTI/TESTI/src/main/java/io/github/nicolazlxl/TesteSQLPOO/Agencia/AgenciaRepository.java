e625c19b2591cbc9ad8573064f61579f0fbf12e8
nicolazlxl <129507617+nicolazlxl@users.noreply.github.com>
2025-07-31 22:48:07 -0300

    Interface grafica conectada com banco

---
+++ TESTI/TESTI/src/main/java/io/github/nicolazlxl/TesteSQLPOO/Agencia/AgenciaRepository.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package io.github.nicolazlxl.TesteSQLPOO.Agencia;
+
+import io.github.nicolazlxl.TesteSQLPOO.Entidades.Repository;
+import java.util.Collection;
+/**
+ *
+ * @author Sarah
+ */
+public class AgenciaRepository extends Repository<Agencia>{
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String getJpqlFindAll() {
+        return "SELECT a FROM Agencia a";
+    }
+
+    @Override
+    public String getJpqlFindById() {
+        return "SELECT a FROM Agencia a WHERE a.id = :id";
+    }
+
+    @Override
+    public String getJpqlDeleteById() {
+        return "DELETE FROM Agencia a WHERE a.id = :id";
+    }
+   
+     @Override
+    public String getJpqlFindTrash() {
+        return "DELETE FROM Agencia a WHERE  a.ToTrash = true";
+    }
+    
+     @Override
+    public String getJpqlEmptyTrash() {
+        return "DELETE FROM Agencia a WHERE a.ToTrash = true";
+    }
+
+    public Collection<? extends Agencia> loadFromTrash() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
