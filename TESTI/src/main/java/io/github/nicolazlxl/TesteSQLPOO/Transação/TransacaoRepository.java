8014ff47331f2ef3fabff9059b7970670a725ff7
Tainara Ribeiro <ribeiros.tai@gmail.com>
2025-08-12 00:16:15 -0300

    Correção de erro nos métodos da lixeira

--- TESTI/src/main/java/io/github/nicolazlxl/TesteSQLPOO/Transação/TransacaoRepository.java
+++ TESTI/src/main/java/io/github/nicolazlxl/TesteSQLPOO/Transação/TransacaoRepository.java
@@ -2,12 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package io.github.nicolazlxl.TesteSQLPOO.Transação;
+package io.github.nicolazlxl.TesteSQLPOO.Transacao;
 
 
 import io.github.nicolazlxl.TesteSQLPOO.Entidades.Repository;
-import java.util.Collection;
-import java.util.List;
+
 /**
  *
  * @author Sarah
@@ -35,24 +34,12 @@ public class TransacaoRepository extends Repository<Transacao>{
    
      @Override
     public String getJpqlFindTrash() {
-        return "DELETE FROM Transacao a WHERE  a.ToTrash = true";
+        return "SELECT a FROM Transacao a WHERE a.ToTrash = true";
     }
-    
-     @Override
+
+    @Override
     public String getJpqlEmptyTrash() {
         return "DELETE FROM Transacao a WHERE a.ToTrash = true";
-    }
-
-    public Collection<? extends Transacao> loadFromTrash() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    public void moveToThash(Transacao selected) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    public void moveToTrash(List<Transacao> selection) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
 }
