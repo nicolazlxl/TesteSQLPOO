dc1bd40e7a568cfee4188f431e86c21a05e7cb3e
nicolazlxl <129507617+nicolazlxl@users.noreply.github.com>
2025-08-12 08:36:16 -0300

    NomeCerto

---
+++ TESTI/src/main/java/io/github/nicolazlxl/TesteSQLPOO/Fatura/FaturaRepository.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package io.github.nicolazlxl.TesteSQLPOO.Fatura;
+
+
+
+import io.github.nicolazlxl.TesteSQLPOO.Entidades.Repository;
+
+/**
+ *
+ * @author taina
+ */
+public class FaturaRepository 
+    extends Repository<Fatura> {
+     @Override
+    public String getJpqlFindAll() {
+        return "SELECT a FROM Fatura a WHERE a.ToTrash = false";
+    }
+
+    @Override
+    public String getJpqlFindById() {
+        return "SELECT a FROM Fatura a WHERE a.id = :id";
+    }
+    
+     @Override
+    public String getJpqlFindTrash() {
+        return "SELECT a FROM Fatura a WHERE a.ToTrash = true";
+    }
+
+    @Override
+    public String getJpqlDeleteById() {
+        return "DELETE FROM Fatura a WHERE a.id = :id";
+    }
+    
+     @Override
+    public String getJpqlEmptyTrash() {
+        return "DELETE FROM Fatura a WHERE a.ToTrash = true";
+    }
+    
+}
+
